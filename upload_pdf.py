02eb30ae10f128554e929c8b885f463f972ec3f0
schlouff <33584736+schlouff@users.noreply.github.com>
2024-07-03 17:06:20 +0200

    client-Zugriff in upload-pdf korrigiert

--- upload_pdf.py
+++ upload_pdf.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 def upload_pdf_to_gcs(bucket_name, source_file, destination_blob_name):
     credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
-    storage_client = storage.Client()
+    storage_client = storage.Client(credentials=credentials, project=credentials.project_id)
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
 
